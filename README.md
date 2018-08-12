5002bb24f5fbccedcce4ece63093854d0e78557b
Phellipe <phellipe.dev@gmail.com>
2018-08-11 23:59:10 -0300

    feat: specs setup
    feat: initial version setup

--- README.md
+++ README.md
@@ -1,85 +1,42 @@
-# Webpack library starter
+# Hierarchical Role-Based Access Control for Node.js
 
-Webpack based boilerplate for producing libraries (Input: ES6, Output: universal library)
-
-![Travis](https://travis-ci.org/krasimir/webpack-library-starter.svg?branch=master)
+* LightWeight
+* Blazzing Fast
+* Zero dependency
 
 ## Features
 
-* Webpack 4 based.
-* ES6 as a source.
-* Exports in a [umd](https://github.com/umdjs/umd) format so your library works everywhere.
-* ES6 test setup with [Mocha](http://mochajs.org/) and [Chai](http://chaijs.com/).
-* Linting with [ESLint](http://eslint.org/).
+* Focused on operations
+* Scalable
+* Each role is given specific access rights for every operation
+* High granularity in assigning rights
 
-## Process
+## Getting Started
 
-```
-ES6 source files
-       |
-       |
-    webpack
-       |
-       +--- babel, eslint
-       |
-  ready to use
-     library
-  in umd format
-```
 
-*Have in mind that you have to build your library before publishing. The files under the `lib` folder are the ones that should be distributed.*
+## Contributing
 
-## Getting started
+#### Contributions are welcome!
 
-1. Setting up the name of your library
-  * Open `webpack.config.js` file and change the value of `libraryName` variable.
-  * Open `package.json` file and change the value of `main` property so it matches the name of your library.
-2. Build your library
-  * Run `yarn install` (recommended) or `npm install` to get the project's dependencies
-  * Run `yarn build` or `npm run build` to produce minified version of your library.
-3. Development mode
-  * Having all the dependencies installed run `yarn dev` or `npm run dev`. This command will generate an non-minified version of your library and will run a watcher so you get the compilation on file change.
-4. Running the tests
+1. Build RBAC
+  * Run `yarn install` to get the RBAC's dependencies
+  * Run `yarn build` to produce minified version of RBAC.
+
+2. Development mode
+  * Having all the dependencies installed run `yarn dev`. This command will generate an non-minified version of your library and will run a watcher so you get the compilation on file change.
+
+3. Running the tests
   * Run `yarn test` or `npm run test`
 
-## Scripts
+4. Scripts
+* `yarn build` - produces production version of your library under the `lib` folder
+* `yarn dev` - produces development version of your library and runs a watcher
+* `yarn test` - well ... it runs the tests :)
+* `yarn test:watch` - same as above but in a watch mode
 
-* `yarn build` or `npm run build` - produces production version of your library under the `lib` folder
-* `yarn dev` or `npm run dev` - produces development version of your library and runs a watcher
-* `yarn test` or `npm run test` - well ... it runs the tests :)
-* `yarn test:watch` or `npm run test:watch` - same as above but in a watch mode
 
-## Readings
+## Thanks
 
-* [Start your own JavaScript library using webpack and ES6](http://krasimirtsonev.com/blog/article/javascript-library-starter-using-webpack-es6)
+  This project scaffold was built with a modified version of [webpack-library-starter](https://github.com/krasimir/webpack-library-starter)
 
-## Misc
-
-### An example of using dependencies that shouldn’t be resolved by webpack, but should become dependencies of the resulting bundle
-
-In the following example we are excluding React and Lodash:
-
-```js
-{
-  devtool: 'source-map',
-  output: {
-    path: '...',
-    libraryTarget: 'umd',
-    library: '...'
-  },
-  entry: '...',
-  ...
-  externals: {
-    react: 'react'
-    // Use more complicated mapping for lodash.
-    // We need to access it differently depending
-    // on the environment.
-    lodash: {
-      commonjs: 'lodash',
-      commonjs2: 'lodash',
-      amd: '_',
-      root: '_'
-    }
-  }
-}
-```
+  Thanks for Karl Düüna ([DeadAlready](https://github.com/DeadAlready)) and his awesome [post on medium](https://blog.nodeswat.com/implement-access-control-in-node-js-8567e7b484d1): 
---
+++ test/rbac.spec.js
@@ -0,0 +1,180 @@
+/* global describe, it, before */
+
+import chai from 'chai';
+import rbac from '../lib/rbac';
+import {
+  USER,
+  ADMIN,
+  SUPERVISOR,
+  SYSADMIN,
+  PRODUCTS_DELETE,
+  PRODUCTS_EDIT,
+  PRODUCTS_FIND
+} from './constants';
+
+chai.expect();
+
+const expect = chai.expect;
+
+const belongsToAccount = (params, done) => done(null, params);
+
+const PromisebelongsToAccount = new Promise((resolve) => {
+  resolve(true);
+});
+
+const defaultRoles = {
+  [USER]: {
+    can: [PRODUCTS_FIND]
+  },
+  [SUPERVISOR]: {
+    can: [{name: PRODUCTS_EDIT, when: PromisebelongsToAccount }],
+    inherits: [USER]
+  },
+  [ADMIN]: {
+    can: [{name: PRODUCTS_DELETE, when: belongsToAccount }],
+    inherits: [SUPERVISOR]
+  },
+  [SYSADMIN]: {
+    can: [PRODUCTS_FIND, PRODUCTS_EDIT, PRODUCTS_DELETE]
+  }
+};
+
+describe('RBAC lib aspects', () => {
+  it('RBAC config should be a function', () => {
+    expect(rbac).to.be.a('function');
+  });
+
+  it('should return correct name', () => {
+    expect(rbac.name).to.be.equal('RBAC');
+  });
+
+  it('should return a function', () => {
+    expect(rbac()).to.be.a('function');
+  });
+
+  it('should return an object with a property named [can] when rbac function is called', () => {
+    expect(rbac()(defaultRoles))
+      .to.be.an('object')
+      .and.to.have.a.property('can');
+  });
+});
+
+describe('RBAC', () => {
+  const RBAC = rbac()(defaultRoles);
+
+  describe('user role', () => {
+    it('[user] should have permission [products:find]', async () => {
+      const result = await RBAC.can(USER, PRODUCTS_FIND);
+
+      expect(result).to.be.true;
+    });
+
+    it('[user] should not have permission [wrong:operation]', async () => {
+      const result = await RBAC.can(USER, 'wrong:operation');
+
+      expect(result).to.be.false;
+    });
+
+    it('[user] should not have [supervisor] permissions [products:edit]', async () => {
+      const result = await RBAC.can(USER, PRODUCTS_EDIT);
+
+      expect(result).to.be.false;
+    });
+
+    it('[user] should not have admin permissions [products:delete]', async () => {
+      const result = await RBAC.can(USER, PRODUCTS_DELETE);
+
+      expect(result).to.be.false;
+    });
+  });
+
+  describe('supervisor role', () => {
+
+    it('[supervisor] should have permission [products:edit]', async () => {
+      const result = await RBAC.can(SUPERVISOR, PRODUCTS_EDIT);
+
+      expect(result).to.be.true;
+    });
+
+    it('[supervisor] should have [user] inherited permission [products:find]', async () => {
+      const result = await RBAC.can(SUPERVISOR, PRODUCTS_FIND);
+
+      expect(result).to.be.true;
+    });
+
+    it('[supervisor] should not have admin permission [products:delete]', async () => {
+      const result = await RBAC.can(SUPERVISOR, PRODUCTS_DELETE);
+
+      expect(result).to.be.false;
+    });
+
+    it('[supervisor] should not have permission [wrong:operation]', async () => {
+      const result = await RBAC.can(SUPERVISOR, 'wrong:operation');
+
+      expect(result).to.be.false;
+    });
+
+  });
+
+  describe('admin role', () => {
+
+    it('[admin] should have permission [products:delete]', async () => {
+      const result = await RBAC.can(ADMIN, PRODUCTS_DELETE, true);
+
+      expect(result).to.be.true;
+    });
+
+    it('[admin] should not have permission if when returns falsy [products:delete]', async () => {
+      const result = await RBAC.can(ADMIN, PRODUCTS_DELETE, false);
+
+      expect(result).to.be.false;
+    });
+
+    it('[admin] should have [user] inherited permission [products:find]', async () => {
+      const result = await RBAC.can(ADMIN, PRODUCTS_FIND);
+
+      expect(result).to.be.true;
+    });
+
+    it('[admin] should have [supervisor] inherited permission [products:edit]', async () => {
+      const result = await RBAC.can(ADMIN, PRODUCTS_EDIT);
+
+      expect(result).to.be.true;
+    });
+
+    it('[admin] should not have permission [wrong:operation]', async () => {
+      const result = await RBAC.can(ADMIN, 'wrong:operation');
+
+      expect(result).to.be.false;
+    });
+
+  });
+
+  describe('sysadmin role', () => {
+
+    it('[sysadmin] should have permission [products:find]', async () => {
+      const result = await RBAC.can(SYSADMIN, PRODUCTS_FIND);
+
+      expect(result).to.be.true;
+    });
+
+    it('[sysadmin] should have permission [products:edit]', async () => {
+      const result = await RBAC.can(SYSADMIN, PRODUCTS_EDIT);
+
+      expect(result).to.be.true;
+    });
+
+    it('[sysadmin] should have permission [products:delete]', async () => {
+      const result = await RBAC.can(SYSADMIN, PRODUCTS_DELETE);
+
+      expect(result).to.be.true;
+    });
+
+    it('[sysadmin] should not have permission [wrong:operation]', async () => {
+      const result = await RBAC.can(SYSADMIN, 'wrong:operation');
+
+      expect(result).to.be.false;
+    });
+
+  });
+});
