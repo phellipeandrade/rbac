f728bb20eab9cd4d2d03c7a9782ffd5cb36d842b
Phellipe Andrade <phellipe.dev@gmail.com>
2025-06-08 05:52:13 -0300

    fix: remove nestjs dependency and use minimal types

--- src/middlewares/nest.types.ts
+++ src/middlewares/nest.types.ts
@@ -1,28 +1,14 @@
-export interface NestHttpExecutionContext {
+export type NestExecutionContext = {
   switchToHttp(): {
     getRequest(): Record<string, unknown>;
     getResponse(): Record<string, unknown>;
     getNext(): (err?: unknown) => void;
   };
-}
-
-export interface NestExecutionContext extends Record<string, unknown> {
-  switchToHttp(): {
-    getRequest(): Record<string, unknown>;
-    getResponse(): Record<string, unknown>;
-    getNext(): (err?: unknown) => void;
-  };
-}
+};
 
 export type NestNextFunction = (err?: unknown) => void;
 
-export type NestHttpResponse = Record<string, unknown> & {
-  status: (code: number) => NestHttpResponse;
+export type NestResponse = {
+  status: (code: number) => any;
   send: (data: string | object) => void;
 };
-
-export interface NestOptions<P = unknown> {
-  getRole?: (req: NestExecutionContext) => string;
-  getParams?: (req: NestExecutionContext) => P;
-  onDenied?: (req: NestExecutionContext, res: NestHttpResponse, next: NestNextFunction) => void;
-}
